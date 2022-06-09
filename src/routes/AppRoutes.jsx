aa49ea09222548cc05b5a54eb77f584b03fcc824
Melisa Mendoza <melisamendozamogollon@gmail.com>
2022-06-09 16:40:08 -0500

    Capturando Login

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Login } from '../containers/Login';
 import Main from '../containers/Main';
 import { Register } from '../containers/Register';
 
@@ -10,6 +11,8 @@ const AppRoutes = () => {
                 <Routes>
                     <Route path='/' element={<Main />} />
                     <Route path='/Register' element={<Register/>} />
+                    <Route path='/Login' element={<Login/>} />
+
                 </Routes>
             </BrowserRouter>
         </div>
